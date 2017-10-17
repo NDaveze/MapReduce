bb7f90be38fdb0f879eb291545d844936ed3a4db
NDaveze <nicolasdaveze@gmail.com>
2017-10-17 23:28:53 +0200

    Pivot map/reduce, desordre dans les colonnes

---
+++ Pivot.java
@@ -0,0 +1,70 @@
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+
+public class Pivot {
+
+  public static class TokenizerMapper
+       extends Mapper<Object, Text, Text, Text>{
+
+    private final static IntWritable one = new IntWritable(1);
+    private Text word = new Text();
+
+    public void map(Object key, Text value, Context context
+                    ) throws IOException, InterruptedException {
+      StringTokenizer itr = new StringTokenizer(value.toString(), ";");
+      while (itr.hasMoreTokens()) {
+        word.set(itr.nextToken());
+        context.write(new Text(one.toString()),word);
+        one.set(one.get()+1);;
+      }
+      one.set(1);
+    }
+  }
+
+  public static class IntSumReducer
+  extends Reducer<Text,Text,Text,Text> {
+                private Text result = new Text();
+
+                public void reduce(Text key, Iterable<Text> values,
+                                  Context context
+                                  ) throws IOException, InterruptedException {
+                        String cell = new String();
+
+
+                        for (Text val : values) {
+                                cell += new String(val.toString());
+                                cell += ";";
+                      }
+
+
+
+                        result.set(new Text(cell.toString()));
+                        context.write(key, result);
+
+                }
+}
+
+  public static void main(String[] args) throws Exception {
+    Configuration conf = new Configuration();
+    Job job = Job.getInstance(conf, "pivot");
+    job.setJarByClass(Pivot.class);
+    job.setMapperClass(TokenizerMapper.class);
+    job.setCombinerClass(IntSumReducer.class);
+    job.setReducerClass(IntSumReducer.class);
+    job.setOutputKeyClass(Text.class);
+    job.setOutputValueClass(Text.class);
+    FileInputFormat.addInputPath(job, new Path(args[0]));
+    FileOutputFormat.setOutputPath(job, new Path(args[1]));
+    System.exit(job.waitForCompletion(true) ? 0 : 1);
+  }
+}
