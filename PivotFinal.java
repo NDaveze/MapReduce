73fd8494ba450135937842d041cbbf0394785ef3
NDaveze <nicolasdaveze@gmail.com>
2017-10-18 21:55:41 +0200

    Pivot map/reduce ok, Run sur le cluster et output hdfs, commentaires

--- PivotFinal.java
+++ PivotFinal.java
@@ -29,47 +29,59 @@ public class PivotFinal {
     
     public void map(LongWritable key, Text value, Context context
                     ) throws IOException, InterruptedException {
-      StringTokenizer itr = new StringTokenizer(value.toString(), ";");
+    	
+    	/*L'input reader envoie les lignes 1 par 1, on les split avec le séparateur ";"*/
+    	StringTokenizer itr = new StringTokenizer(value.toString(), ";");
       
-      MapWritable map = new MapWritable();
-      
-      int col = 1;
-      
-      while (itr.hasMoreTokens()) {
-        map.put(row, new Text(itr.nextToken()));
-        context.write(new LongWritable(col),map);
-        col ++;
-      }
-      row.set(row.get()+1);
+    	/*On créé une map (k,v) pour stocker le numero de ligne + valeur de la case lue*/
+    	MapWritable map = new MapWritable();
+    	
+    	
+    	int col = 1;
+	    /*On parcourt les valeurs de la ligne en cours*/
+    	while (itr.hasMoreTokens()) {
+    		/*On insert dans map num de ligne + valeur de chaque case*/
+	        map.put(row, new Text(itr.nextToken()));
+	        /*On transmet au reducer la colonne de chaque case*/
+	        context.write(new LongWritable(col),map);
+	        col ++;
+	    }
+    	/*On change de ligne*/
+	    row.set(row.get()+1);
     }
   }
-
+  
   public static class IntSumReducer
   extends Reducer<LongWritable, MapWritable, LongWritable, Text> {
                 private Text result = new Text();
 
                 public void reduce(LongWritable key, Iterable<MapWritable> maps, Context context
                                   ) throws IOException, InterruptedException {
+                	/*On créé une liste de map pour lire maps*/
                 	SortedMap<IntWritable,Text> rowVals = new TreeMap<IntWritable,Text>();
                 	StringBuffer line = new StringBuffer();
                 	
+                	/*On parcourt les couples k,v dans maps*/
                 	for (MapWritable map : maps) {
                 		for(Entry<Writable, Writable>  entry : map.entrySet()) {
+                			/*On écrit dans rowvals les valeurs des cases de chaque colonne*/
                             rowVals.put((IntWritable)entry.getKey(),(Text) entry.getValue());
                 		}
                 	}
                 	
+                	/*On recopie ces valeurs dans une string, qu'on envoie en output*/
                 	for(Text rowVal : rowVals.values()) {
           	          line.append(rowVal.toString());
           	          line.append(";");
           	        }
-                	
+                	line.delete((line.length() - 1), line.length());
                 	context.write(key,new Text(line.toString()));
                 }
 }
 
   public static void main(String[] args) throws Exception {
     Configuration conf = new Configuration();
+    
     Job job = Job.getInstance(conf, "pivotfinal");
     
     job.setJarByClass(PivotFinal.class);
@@ -78,7 +90,9 @@ public class PivotFinal {
     job.setOutputValueClass(MapWritable.class);
     
     job.setMapperClass(TokenizerMapper.class);
-    
+    /*l'output est de la forme (colonne,(ligne,valeur))*/
+    /*Pas de combiner, car le mapper ne renvoie que des clés uniques*/
+    /*Le shuffle and sort renvoie */
     job.setReducerClass(IntSumReducer.class);
     
     FileInputFormat.addInputPath(job, new Path(args[0]));
