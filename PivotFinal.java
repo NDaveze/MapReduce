8f2ca670f4b010babbd10369502c15f913098eba
NDaveze <nicolasdaveze@gmail.com>
2017-10-18 20:11:19 +0200

    Pivot map/reduce ok, Run sur le cluster et output hdfs

--- PivotFinal.java
+++ PivotFinal.java
@@ -1,4 +1,5 @@
 import java.io.IOException;
+import java.util.Map.Entry;
 import java.util.SortedMap;
 import java.util.StringTokenizer;
 import java.util.TreeMap;
@@ -9,11 +10,14 @@ import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.MapWritable;
 import org.apache.hadoop.io.Text;
+import org.apache.hadoop.io.Writable;
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.Mapper;
 import org.apache.hadoop.mapreduce.Reducer;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
 
 public class PivotFinal {
 
@@ -21,7 +25,7 @@ public class PivotFinal {
        extends Mapper<LongWritable, Text, LongWritable, MapWritable>{
 
     private Text word = new Text();
-    private final static IntWritable row = new IntWritable(1);
+    private final static IntWritable row = new IntWritable(0);
     
     public void map(LongWritable key, Text value, Context context
                     ) throws IOException, InterruptedException {
@@ -32,11 +36,11 @@ public class PivotFinal {
       int col = 1;
       
       while (itr.hasMoreTokens()) {
-        map.put(new Text(row.toString()), new Text(itr.nextToken()));
+        map.put(row, new Text(itr.nextToken()));
         context.write(new LongWritable(col),map);
         col ++;
       }
-      row.set(1);
+      row.set(row.get()+1);
     }
   }
 
@@ -46,36 +50,43 @@ public class PivotFinal {
 
                 public void reduce(LongWritable key, Iterable<MapWritable> maps, Context context
                                   ) throws IOException, InterruptedException {
-                	SortedMap<LongWritable,IntWritable> rowVals = new TreeMap<LongWritable,IntWritable>();
-                	String cell = new String();
-
+                	SortedMap<IntWritable,Text> rowVals = new TreeMap<IntWritable,Text>();
+                	StringBuffer line = new StringBuffer();
+                	
                 	for (MapWritable map : maps) {
                 		for(Entry<Writable, Writable>  entry : map.entrySet()) {
-                            rowVals.put((LongWritable) entry.getKey(),(IntWritable) entry.getValue());
-                }
-                            cell += new String(val.toString());
-                            cell += ";";
-                    }
-
-
-
-                        result.set(new Text(cell.toString()));
-                        context.write(key, result);
-
+                            rowVals.put((IntWritable)entry.getKey(),(Text) entry.getValue());
+                		}
+                	}
+                	
+                	for(Text rowVal : rowVals.values()) {
+          	          line.append(rowVal.toString());
+          	          line.append(";");
+          	        }
+                	
+                	context.write(key,new Text(line.toString()));
                 }
 }
 
   public static void main(String[] args) throws Exception {
     Configuration conf = new Configuration();
     Job job = Job.getInstance(conf, "pivotfinal");
-    job.setJarByClass(Pivot.class);
+    
+    job.setJarByClass(PivotFinal.class);
+    
+    job.setOutputKeyClass(LongWritable.class);
+    job.setOutputValueClass(MapWritable.class);
+    
     job.setMapperClass(TokenizerMapper.class);
-    job.setCombinerClass(IntSumReducer.class);
+    
     job.setReducerClass(IntSumReducer.class);
-    job.setOutputKeyClass(Text.class);
-    job.setOutputValueClass(Text.class);
+    
     FileInputFormat.addInputPath(job, new Path(args[0]));
     FileOutputFormat.setOutputPath(job, new Path(args[1]));
+    
+    job.setInputFormatClass(TextInputFormat.class);
+    job.setOutputFormatClass(TextOutputFormat.class);
+    
     System.exit(job.waitForCompletion(true) ? 0 : 1);
   }
 }
